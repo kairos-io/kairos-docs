8122c34982b91d2aed448960d396fbd5cf79f8a0
Mircea Bozianu <mircea@spectrocloud.com.com>
2023-08-08 13:03:11 +0300

    Created modal for ARM links
    Signed-off-by: Mauro Morales <mauro.morales@spectrocloud.com>

--- static/js/services.js
+++ static/js/services.js
@@ -1,4 +1,18 @@
-const HTML_TAGS = ["h1", "h2", "h3", "h4" ,"div", "p" ,"span", "a", "img" ,"i"]
+const HTML_TAGS = [
+    "h1",
+    "h2",
+    "h3",
+    "h4" ,
+    "div",
+    "p" ,
+    "span",
+    "a",
+    "img" ,
+    "i",
+    "dialog",
+    "pre",
+    "button",
+]
 
 export function renderHTMLTree(dataTree = {}, rootElem){
     Object.keys(dataTree).forEach(treeKey => {
@@ -18,6 +32,10 @@ export function renderHTMLTree(dataTree = {}, rootElem){
             }
 
             rootElem.appendChild(elem)
+        }
+
+        if (Object.hasOwn(dataTree[treeKey], "id")){
+            elem.setAttribute("id", dataTree[treeKey].id)
         }
 
         if (Object.hasOwn(dataTree[treeKey], "class")){
