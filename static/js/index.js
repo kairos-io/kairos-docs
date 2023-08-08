43e79a219adbf8e66f050da32ac2083b06d41aa5
Mircea Bozianu <mircea@spectrocloud.com.com>
2023-08-08 13:03:11 +0300

    Created modal for ARM links

--- static/js/index.js
+++ static/js/index.js
@@ -45,3 +45,35 @@ BLADES.forEach(({id, tree})=> {
     const rootElem = document.getElementById(id)
     renderHTMLTree(tree, rootElem)
 })
+
+const INTEGRATIONS = ["ubuntu", "fedora", "alpine"]
+
+INTEGRATIONS.forEach(integration => {
+    const modal = document.querySelector(`#${integration}-modal`)
+    const openModalButton = document.querySelector(`.open-dialog-${integration}`)
+    const closeModalButton = document.querySelector(`.close-dialog-${integration}`)
+    const copyToClipboardButton = document.querySelector(`.copy-to-clipboard-${integration}`,)
+    const codeToCopy = document.querySelector(`.code-${integration}`)
+    const copiedNotification = document.querySelector(`.copied-${integration}`)
+
+    copyToClipboardButton.addEventListener("click", (e) => {
+        if(!codeToCopy.innerHTML){
+            return
+        }
+        e.preventDefault();
+        navigator.clipboard.writeText(codeToCopy.innerHTML)
+        copiedNotification.style.setProperty("display", "block");
+    });
+
+    openModalButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        modal.showModal();
+    });
+
+    closeModalButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        modal.close()
+        copiedNotification.style.setProperty("display", "none");
+    });
+
+})
