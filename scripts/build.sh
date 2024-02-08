3459fa349039a7c129c9b33e4aba1f3af5385521
Mauro Morales <mauro.morales@spectrocloud.com>
2024-02-08 16:45:51 +0100

    just fetch

--- scripts/build.sh
+++ scripts/build.sh
@@ -25,7 +25,7 @@ rm -rf "${publicpath}" || true
 npm install --save-dev autoprefixer postcss-cli postcss
 
 # list all branches to debug
-git fetch origin 'v*'
+git fetch
 git branch
 # get all release branches
 releases=$(git branch | grep -E 'release_v[0-9]+\.[0-9]+\.[0-0]+.?')
